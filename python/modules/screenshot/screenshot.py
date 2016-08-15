7f43dcc239524b3dfd41d68a0b32828606f88dfe
Eran Goldman-Malka <erangoldman@gmail.com>
2016-08-15 14:24:25 +0300

    stated a new python server because it's easier to install...

---
+++ python/modules/screenshot/screenshot.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+"""This script take screenshot and saves it int eh screenshots folder."""
+import datetime
+import wx
+import os
+
+
+def screenshot():
+    """Take a screenshot."""
+    # folder_name = datetime.date.today().strftime('%Y-%m-%d')
+    file_name = datetime.datetime.now().strftime('%H-%M-%S') + '.png'
+    # print (folder_name)
+    # print(file_name)
+    directory = os.path.join(os.path.dirname(os.getcwd()), 'nodeServer',
+                             'public', 'images', 'screenshots')
+    # print(directory)
+    # folder_name)
+    # if os.path.exists(directory):
+    #    os.makedirs(directory)
+    filename = os.path.join(directory, file_name)
+    # print(filename)
+    app = wx.App()
+    screen = wx.ScreenDC()
+
+    size = screen.GetSize()
+    width = size[0]
+    height = size[1]
+    bmp = wx.EmptyBitmap(width, height)
+    mem = wx.MemoryDC(bmp)
+    mem.SelectObject(bmp)
+    mem.Blit(0, 0, width, height, screen, 0, 0)
+    mem.SelectObject(wx.NullBitmap)
+    img = bmp.ConvertToImage()
+    img.SaveFile(filename, wx.BITMAP_TYPE_PNG)
+    del mem
+    # bmp.SaveFile(filename, wx.BITMAP_TYPE_PNG)
+
+    return (file_name)
+
+print(screenshot())
